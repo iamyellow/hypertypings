f59e65e5915e3226da043d5931cc030ed5c239fe
jordi domenech <jordi@iamyellow.net>
2024-09-12 15:48:50 +0200

    add \n to index.d.ts

--- build.js
+++ build.js
@@ -5,4 +5,4 @@ const FILTER = /^(?!(index\.)).*\.d\.ts$/
 const files = fs.readdirSync('.').filter(f =>  FILTER.test(f))
   .map(f => `/// <reference path="${f}" />`)
   .join('\n')
-fs.writeFileSync('index.d.ts', files)
+fs.writeFileSync('index.d.ts', `${files}\n`)
